9603c685b4d6153d07bb76354697c9d2b1cb86c6
Aryesh Srivastava <aryeshsrivastava@gmail.com>
2024-03-27 23:28:28 +0530

    last commit

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import '../Style/Contact.css'
 
 function Contact() {
   const name = useRef();
@@ -13,15 +14,15 @@ function Contact() {
     setFinalmail(mail.current.value);
   }
   return (
-    <div>
-      <form>
-        <input type="text" placeholder='Please Enter Your Name' ref={name}/>
+    <div className='contact-div'>
+      <form className='contact-form'>
+        <input type="text" placeholder='Please Enter Your Name' ref={name} className='contact-input'/>
         <br />
-        <input type="email" placeholder='Please Enter Your Mail Address' ref={mail}/>
+        <input type="email" placeholder='Please Enter Your Mail Address' ref={mail} className='contact-input'/>
         <br />
-        <button onClick={submitdata}>Submit</button>
+        <button onClick={submitdata} className='contact-submit'>Submit</button>
       </form>
-      <h3><u>For Display Purpose Only.</u></h3>
+      <h3><u>For Display Purpose Only :</u></h3>
       <h3>Name : {finalname}</h3>
       <h3>Mail : {finalmail}</h3>
     </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import bgimg from '../Images/bg-img.avif'
+import '../Style/Home.css'
 
 function Home() {
+
+  const mail = useRef();
+
+  const [finalmail, setFinalmail] = useState();
+
+  function submitdata(event){
+    event.preventDefault();
+    setFinalmail(mail.current.value);
+    alert(finalmail)
+  }
   return (
     <div>
-      
+      <div className='home-div'>
+        <img src={bgimg} alt="" className='home-img'/>
+        <div className='home-text'>
+          <h2>Connecting Brands with Creativity</h2>
+          <h4>Welcome to Mediater - Your Gateway to Digital Collaboration!</h4>
+          <p>Elevate Your Experience: Subscribe Now for Exclusive Early Access!</p>
+          <form className='home-form'>
+          <input type="email" placeholder='Please Enter Your Mail Address' ref={mail} className='home-input'/>
+          <br />
+          <button onClick={submitdata} className='home-join'>Join Now</button>
+        </form>
+        </div>
+      </div>
     </div>
   )
 }
